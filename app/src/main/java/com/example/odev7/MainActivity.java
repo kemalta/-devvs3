8631878a88b22b0effd8701dea8625510cc45919
kemalta <100770665+kemalta@users.noreply.github.com>
2022-12-27 22:55:16 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/odev7/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.odev7;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import com.example.odev7.databinding.ActivityMainBinding;
+
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+    public void digerActivity(View view){
+        String adiSoyadi = binding.editTextTextAdiSoyadi.getText().toString();
+        String eMail= binding.editTextTextEmail.getText().toString();
+        String telefonNo= binding.editTextTelefon.getText().toString();
+        Bilgiler bilgilerSerializable = new Bilgiler(adiSoyadi,eMail,telefonNo);
+        Intent intent = new Intent(this.getApplicationContext(),Activity2.class);
+        intent.putExtra("bilgiler",bilgilerSerializable);
+        startActivity(intent);
+    }
+}
